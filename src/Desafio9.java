1b4060c720bd1c733e51284cd31f6064b503bded
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2022-07-12 12:54:10 -0300

    Nova implementação do desafio 9

--- src/Desafio9.java
+++ src/Desafio9.java
@@ -1,27 +1,44 @@
-﻿import viagem.Routes;
+﻿
+import viagem.Map;
+import viagem.Route;
 
 public class Desafio9 {
 
     public static void main(String[] args) {
 
-        Routes mapaRotas = new Routes(8);
+        Map mapaRotas = new Map(8);
 
-        mapaRotas.adicionarAresta(0, 1);
-        mapaRotas.adicionarAresta(0, 3);
-        mapaRotas.adicionarAresta(0, 6);
-        mapaRotas.adicionarAresta(1, 3);
-        mapaRotas.adicionarAresta(1, 5);
-        mapaRotas.adicionarAresta(2, 1);
-        mapaRotas.adicionarAresta(4, 1);
-        mapaRotas.adicionarAresta(6, 3);
-        mapaRotas.adicionarAresta(6, 5);
-        mapaRotas.adicionarAresta(6, 7);
-        mapaRotas.adicionarAresta(6, 0);
-        mapaRotas.adicionarAresta(7, 1);
+        Route route1 = new Route("BSB", "MON");
+        mapaRotas.adicionarAresta(route1);
 
-        int s = 0, d = 5, r = 0;
-        r = mapaRotas.countPaths(s, d, 2);
-        System.out.println(r);
+        Route route2 = new Route("RIC", "BAR");
+        mapaRotas.adicionarAresta(route2);
+
+        // mapaRotas.adicionarAresta(0, 6);
+
+        // mapaRotas.adicionarAresta(1, 3);
+
+        // mapaRotas.adicionarAresta(1, 5);
+
+        // mapaRotas.adicionarAresta(2, 1);
+
+        // mapaRotas.adicionarAresta(4, 1);
+
+        // mapaRotas.adicionarAresta(6, 3);
+
+        // mapaRotas.adicionarAresta(6, 5);
+
+        // mapaRotas.adicionarAresta(6, 7);
+
+        // mapaRotas.adicionarAresta(6, 0);
+
+        // mapaRotas.adicionarAresta(7, 1);
+
+        // int s = 0, d = 5, r = 0;
+        // r = mapaRotas.countPaths(s, d, 2);
+        // System.out.println(r);
+
+        mapaRotas.imprimirMapa();
 
     }
 
---
+++ src/viagem/Route.java
@@ -0,0 +1,40 @@
+﻿package viagem;
+
+public class Route {
+
+    private Integer originNodeIndex;
+    private String origin;
+    private String destiny;
+
+    public Route(String origin, String destiny) {
+
+        setOrigin(origin);
+        setDestiny(destiny);
+
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestiny() {
+        return destiny;
+    }
+
+    public void setDestiny(String destiny) {
+        this.destiny = destiny;
+    }
+
+    public Integer getOriginNodeIndex() {
+        return originNodeIndex;
+    }
+
+    public void setOriginNodeIndex(Integer originNodeIndex) {
+        this.originNodeIndex = originNodeIndex;
+    }
+
+}

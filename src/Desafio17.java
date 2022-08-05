e2a77eab0c3537153b544557a84d40686ffd57a5
F9539591 Vinicius de Sousa Santana <vinicius.ssantana@bb.com.br>
2022-08-05 17:30:52 -0300

    Iniciando a implementação do desafio 17

--- src/Desafio17.java
+++ src/Desafio17.java
@@ -1,3 +1,16 @@
+import java.util.regex.Pattern;
+
 public class Desafio17 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Boolean linkCheck(String link) {
+
+        Pattern linkPattern = Pattern.compile("[a-z]+");
+
+        return true;
+    }
 
 }
